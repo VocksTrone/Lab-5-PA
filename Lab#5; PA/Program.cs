04bac3c3f2c94ac87be2644624d5287f435cf44b
VocksTrone <jr.alvarez0316@gmail.com>
2024-09-04 09:07:37 -0600

    Creación del Menú Inicial

--- Lab#5; PA/Program.cs
+++ Lab#5; PA/Program.cs
@@ -1,3 +1,16 @@
 ﻿using Lab_5__PA;
 
 List <Homeworks> homeworksList = new List <Homeworks>();
+int MenuHomeworks()
+{
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine("\n--- Sistema de Control de Tareas ---");
+    Console.ResetColor();
+    Console.WriteLine("1. Agregar Tareas");
+    Console.WriteLine("2. Mostrar Tareas");
+    Console.WriteLine("3. Cambiar Estado");
+    Console.WriteLine("4. Salir");
+    Console.Write("\nIngrese una Opción: ");
+    int optionMenuHomeworks = int.Parse(Console.ReadLine());
+    return optionMenuHomeworks;
+}
