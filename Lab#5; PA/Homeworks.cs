ff065b789cdf7253532014121c5ca9e5258a9f6e
VocksTrone <jr.alvarez0316@gmail.com>
2024-09-04 09:07:19 -0600

    Creación del Método Añadir Tareas

--- Lab#5; PA/Homeworks.cs
+++ Lab#5; PA/Homeworks.cs
@@ -8,5 +8,28 @@ namespace Lab_5__PA
 {
     public class Homeworks
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Homeworks(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+        public void AddHomeworks(ref List<Homeworks> homeworksList)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("--- Agregar Tareas ---");
+            Console.ResetColor();
+            Console.Write("\nIngrese el Nombre de la Tarea: ");
+            string name = Console.ReadLine();
+            Console.Write("\nIngrese la Descripción de la Tarea: ");
+            string description = Console.ReadLine();
+            homeworksList.Add(new Homeworks(name, description));
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Tarea Agregada Correctamente");
+            Console.ResetColor();
+            Console.ReadKey();
+        }
     }
 }
